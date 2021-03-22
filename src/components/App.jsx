8c577cd3e5114b4299113f0d29ea460c245e53de
NilsonKr <nuotrok444@gmail.com>
2021-03-21 22:19:16 -0500

    :sparkles: [ADD] Basic Styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 import Card from './Card';
 
 import '../styles/App.css';
+import facebook from '../images/icon-facebook.svg';
+import pinterest from '../images/icon-pinterest.svg';
+import instagram from '../images/icon-instagram.svg';
 
 const App = () => {
 	return (
@@ -20,6 +23,24 @@ const App = () => {
 					<Card title='Secs' counter='35' />
 				</div>
 			</main>
+			<footer className='app__footer'>
+				<div className='footer__media'>
+					<img src={facebook} alt='' />
+					<img src={pinterest} alt='' />
+					<img src={instagram} alt='' />
+				</div>
+				<div className='footer__credits'>
+					Challenge from
+					<a href='https://www.frontendmentor.io/challenges' target='_blank'>
+						FrontendMentor.io
+					</a>
+					Coded by
+					<a href='https://twitter.com/Nilson_Kr' target='_blank'>
+						NilsonKr
+					</a>
+					with 💚
+				</div>
+			</footer>
 		</div>
 	);
 };
